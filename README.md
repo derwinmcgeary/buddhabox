1176ab50a6dcab9e01d3d55fb2ed196083458d10
Derwin McGeary <derwinmcgeary@gmail.com>
2014-05-08 03:12:52 +0600

    Updated README.md so I don't forget

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-201405080137 Welcome! We have now started a repository for buddhabox
+201405080310 OK, process is, develop and test on home machine, heroku for staging, and heroku production
+
+201405080137 Welcome! We have now started a repository for buddhabox
--- web.js
+++ web.js
@@ -2,13 +2,15 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var PWD = process.cwd();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
+app.use(express.static(PWD + '/'));
+/* app.get('/', function(req, res) {
   res.send('Hello World! Testing devops...');
 });
-
+*/
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
